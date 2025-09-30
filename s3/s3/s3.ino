f8116c8694be23c11b0364c85d1a3eb9836e5e22
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-09-30 10:09:09 -0300

    Update s3.ino

--- s3/s3/s3.ino
+++ s3/s3/s3.ino
@@ -1,4 +1,16 @@
+#include <WiFi.h>
+WiFiClient client; //nome do cliente wifi
+const String SSID = "nome da rede"; //nome da rede wifi
+const String PASS = "senha da rede"; //senha da rede
+
 void setup() {
+  Serial.begin(115200);
+  client.begin(nome, senha); //tenta conectar na rede
+  Serial.println("Conectado no WiFi");
+  while(client.status()! = WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
   // put your setup code here, to run once:
 
 }
