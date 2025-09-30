e4543d1b15d454040eed09e0e04a3b736ef41647
miguelrcha <miguellrochaxavier@gmail.com>
2025-09-30 10:07:40 -0300

    Atualizando S4

--- S4/sketch_sep30a/sketch_sep30a.ino
+++ S4/sketch_sep30a/sketch_sep30a.ino
@@ -1,6 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+WiFiClient client; // Carinha que conecta com o Wi-fi
+const String SSID = "nome da rede";
+const String PASS = "senha da rede";
 
+void setup() {
+  // Função para verificar se o Wi-fi foi conectado com sucesso
+  Serial.begin(115200);
+  client.begin(SSID, PASS);
+  Serial.println("Conectando no Wi-fi");
+  while(client.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(200);
+  }
+  // put your setup code here, to run once:
 }
 
 void loop() {
