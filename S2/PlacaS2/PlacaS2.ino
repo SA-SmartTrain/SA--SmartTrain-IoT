e5a3493d40ab2f777947be34ed5c464f82abee7a
iasmin-rabelo20 <iasmin_rabelo@estudante.sesisenai.org.br>
2025-10-07 09:54:04 -0300

    add pasta S2

---
+++ S2/PlacaS2/PlacaS2.ino
@@ -0,0 +1,39 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient wifi_client;
+PubSubClient mqtt(wifi_client);
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "" ;
+const String brokerPass = "" ;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASS);
+  Serial.println("Conectando no WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso!");
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  String clientID = "S2";
+  clientID += String(random(0xffff),HEX);
+  while (mqtt.connect(clientID.c_str()) == 0){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado ao Broker!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
