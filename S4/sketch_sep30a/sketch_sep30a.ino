6524ced0809a80857ab5c9c966ce5c8f617260d2
miguelrcha <miguellrochaxavier@gmail.com>
2025-10-14 09:54:56 -0300

    Updating S4 Folder with Chat-MQTT

--- S4/sketch_sep30a/sketch_sep30a.ino
+++ S4/sketch_sep30a/sketch_sep30a.ino
@@ -1,43 +1,63 @@
-#include <WiFi.h> // WiFi library for ESP32 DEVKIT V1
-#include <PubSubClient.h> // Client library for MQTT messasing
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 WiFiClient wifi_client;
 PubSubClient mqtt(wifi_client);
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+const String SSID = "FIESC_IOT_EDU";  
+const String PASS = "8120gv08";       
 
-// Declaração Broker Fixa
-const String brokerURL = "test.mosquitto.org"; // Pública agora | SA diferente;
-const int brokerPort = 1883; // Pública agora | SA diferente;
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+const String topic = "Miguel";
 
 const String brokerUser = "";
 const String brokerPass = "";
 
 void setup() {
-  // put your setup code here, to run once:
-  // Função para verificar se o Wi-fi foi conectado com sucesso
   Serial.begin(115200);
-  WiFi.begin(SSID, PASS);
-  Serial.println("Conectando no Wi-fi");
-  while(WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(SSID, PASS);  
+  Serial.println("Conectando no WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
   Serial.println("Conectado com sucesso!");
-  // Conexão com o broker
-  mqtt.setServer(brokerURL.c_str(),brokerPort);
-  String clientID = "S4-";
-  clientID += String(random(0xffff),HEX);
-  Serial.println("Conectando ao Broker");
-  while(mqtt.connect(clientID.c_str()) == 0) {
-    Serial.println(".");
-    delay(200);
+
+  mqtt.setServer(brokerURL.c_str(), brokerPort);
+  String clientID = "S4_MiguelRocha";
+  clientID += String(random(0xffff), HEX);
+  while (mqtt.connect(clientID.c_str()) == 0) {
+    Serial.print(".");
+    delay(2000);
   }
-  Serial.println("\nConectado com sucesso, broker!");
+  mqtt.subscribe(topic.c_str());
+  mqtt.setCallback(callback);
+  Serial.println("\nConectado ao broker!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //Código para enviar para o servidor
+  String mensagem = "";                      
+  if (Serial.available() > 0) {               
+    mensagem = Serial.readStringUntil('\n');  
+    mensagem = "Miguel: " + mensagem;       
+    mqtt.publish("beatrizcercal",mensagem.c_str());
+  }
+  mqtt.loop();
 }
+
+
+void callback(char* topic, byte* payload, unsigned long length) {
+  String mensagem_recebida = "";
+  for(int i=0; i< length; i++){
+    mensagem_recebida += (char) payload[i]; // Agrupa cada caracter recebida func payload[variavel]
+  }
+  Serial.println(mensagem_recebida); // Colocar o LED Pino2
+}
+
+void connecttoBroker(){
+  Serial.println("Conectando ao broker...");
+}
+
